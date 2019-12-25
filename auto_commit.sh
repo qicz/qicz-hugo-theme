606856b96c8320d578a259627bcaad8fec9b55c6
Jobsz <brucezcq@gmail.com>
2019-12-26 01:13:07 +0800

    Site updated at 2019/12/26-01:13:07

---
+++ auto_commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cur_date="`date +%Y/%m/%d-%H:%M:%S`" 
+commit_content="Site updated at "${cur_date}
+
+echo "pushing to master..."
+git add .
+git commit -m "`echo ${commit_content}`"
+git push origin master
